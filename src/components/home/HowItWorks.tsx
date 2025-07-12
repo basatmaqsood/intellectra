b444ac35ef7bdfcdc224b4baff6d1889808b9437
Basat <basat.maqsood@lantrotech.com>
2025-07-13 02:08:19 +0500

    Circles fixed

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -74,7 +74,7 @@ const HowItWorks = () => {
         </div>
         
         {/* Teal gradient ball - top right */}
-        <div className="absolute top-0 right-0 w-36 h-36 sm:w-48 sm:h-48 md:w-60 md:h-60 lg:w-72 lg:h-72 xl:w-84 xl:h-84 rounded-full translate-x-3/8 translate-y-10 blur-lg"
+        <div className="absolute top-0 right-0 w-36 h-36 sm:w-48 sm:h-48 md:w-60 md:h-60 lg:w-72 lg:h-72 xl:w-84 xl:h-84 rounded-full translate-x-5/8 md:translate-x-3/8 translate-y-4 md:translate-y-10 blur-lg"
              style={{
                background: 'radial-gradient(circle at center, #7EC8C4 0%, #7EC8e4 10%, #000000 100%)'
              }}>

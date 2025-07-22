dd8854d701ea2e8079a39137ec196b4ac2189b4d
Basat <basat.maqsood@lantrotech.com>
2025-07-23 00:19:34 +0500

    SCROLL TOP FIXED

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from "./components/ScrollTop.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
 <BrowserRouter>
+    <ScrollToTop />
     <App />
 </BrowserRouter>
   </StrictMode>
